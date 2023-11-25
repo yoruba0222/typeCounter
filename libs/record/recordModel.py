c206e6601691e4c5f36ea1de372b4a4e7a8e69a6
大田啓人 <i19oota@ootakeininnoMacBook-Air.local>
2023-11-26 00:45:06 +0900

    空白行にインデントあるんだけど！😠とりまrecordModel.pyのやつだけ消したけど他のファイルも消さなきゃかも！！！😡

--- libs/record/recordModel.py
+++ libs/record/recordModel.py
@@ -9,7 +9,7 @@ FILENAME: Final[str] = "./data.pb"
 class Record():
     """Record
     過去の記録の読み書きを担当する
-    
+
     Attribute:
         __tc (typeCounter_pb2.typeCount): pbのクラス
         __todayCount (int): 今日タイプした回数
@@ -21,7 +21,7 @@ class Record():
         """
         self.__tc: typeCounter_pb2.typeCount = typeCounter_pb2.typeCount()
         self.__todayCount: int = 0
-        
+
         try:
             # ファイルが存在しているなら読み込んじゃう
             __f = open(FILENAME, "rb")
@@ -34,13 +34,13 @@ class Record():
             self.__tc.count = 0
             __feed_data: str = self.__tc.SerializeToString()
             __f.write(__feed_data)
-            
+
         finally:
             __f.close()
-    
+
     # get past record by date (yyyy-mm-dd)
     def getPastRecord(self, date: str) -> dict:
         pass
-    
+
     def saveRecord(self, data: dict) -> None:
         pass
